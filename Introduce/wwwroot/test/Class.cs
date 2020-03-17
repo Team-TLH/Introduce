3c9742ef1b6cd8adae5198a7fe5af5eea2f2b186
=Anh Tu <=anhtu17698@gmail.com>
2020-03-17 16:57:38 +0700

    add class test

---
+++ Introduce/wwwroot/test/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Introduce.wwwroot.test
+{
+    public class Class
+    {
+        private djfsakdjflasjdfls
+    }
+}
